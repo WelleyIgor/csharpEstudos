8bec78a477482bcf023d5ead41b7c8a9c68f602d
WelleyIgor <122416707+WelleyIgor@users.noreply.github.com>
2023-08-14 19:03:37 -0400

    Add files via upload

---
+++ Projetos/PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 10.38600;
+            int Idade = 29;
+            string Nome = "Welley";
+            string Sexo = "Masculino";
+            Console.WriteLine(x);
+            Console.WriteLine(x.ToString("F1"));
+            Console.WriteLine(x.ToString("F2"));
+            Console.WriteLine("O Valor do troco é " + x.ToString("F3") + " Reais");
+            Console.WriteLine("O Paciente " + Nome + " Tem " + Idade + " anos, e é do sexo: " + Sexo);
+        }
+    }
+}
