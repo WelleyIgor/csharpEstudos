f643f3e794b7744eb98302ab15fcba531d9abae7
WelleyIgor <122416707+WelleyIgor@users.noreply.github.com>
2023-08-14 20:09:17 -0400

    Add files via upload

---
+++ Projetos/exercicioFixaxaoConversao/exercicioFixaxaoConversao/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioConversao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo: ");
+            string nomeCompleto = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa? ");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço do produto:");
+            double precoProduto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu ultimo nome, idade e altura (tudo na mesma linha): ");
+            string[] vetorInfo = Console.ReadLine().Split(' ');
+            string ultimoNome = vetorInfo[0];
+            int idade = int.Parse(vetorInfo[1]);
+            double altura = double.Parse(vetorInfo[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(quartos);
+            Console.WriteLine(precoProduto.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
+
