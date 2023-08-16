0044ef1f9967f68a9743c04a7b00a61b6b77f0e5
WelleyIgor <122416707+WelleyIgor@users.noreply.github.com>
2023-08-15 20:02:14 -0400

    Add files via upload

---
+++ calculoTerrenoEx/calculoTerrenoEx/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace calculadoraTerrenoEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculo de m² de um terreno!");
+            Console.WriteLine("Digite a largura do terreno: ");
+            double largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o comprimento do terreno: ");
+            double comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o valor do m²: ");
+            double valorMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area = (largura * comprimento);
+            double valorTotal = (area * valorMetro);
+
+            Console.WriteLine("Valor da área: " + area.ToString("F1", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor do m²: " + valorTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
