a3c16c546f908a1f39f239b3d3d17163d25de23b
WelleyIgor <122416707+WelleyIgor@users.noreply.github.com>
2023-08-16 19:59:35 -0400

    Add files via upload

---
+++ Projetos/exercicioDois/exercicioDois/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioPropostoDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseTerreno, alturaTerreno, areaTerreno, perimetroTerreno, diagonalTerreno;
+
+            Console.WriteLine("Ola, seja bem vindo!");
+            Console.WriteLine("Digite o valor da Base do terreno: ");
+            baseTerreno = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite a largura do terreno: ");
+            alturaTerreno = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //calculos
+            areaTerreno = baseTerreno * alturaTerreno;
+            perimetroTerreno = (2 * baseTerreno) + (2 * alturaTerreno);
+            diagonalTerreno = Math.Sqrt(Math.Pow(baseTerreno, 2) + Math.Pow(alturaTerreno, 2));
+
+            //resultados
+            Console.WriteLine("Area Total: " + areaTerreno.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro Total: " + perimetroTerreno.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal Total: " + diagonalTerreno.ToString("F4", CultureInfo.InvariantCulture));
+        }
+
+    }
+}
