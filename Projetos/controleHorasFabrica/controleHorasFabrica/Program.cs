626936dd4a00208fbc81171e9496d2da8e4cea1e
WelleyIgor <122416707+WelleyIgor@users.noreply.github.com>
2023-08-16 20:01:42 -0400

    Add files via upload

---
+++ Projetos/controleHorasFabrica/controleHorasFabrica/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace calculadoraHorasFabrica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroSegundos, horas,minutos, segundos, resto;
+
+            Console.WriteLine("Controle da Fabrica!!!");
+            Console.WriteLine("Digite a quantidade de segundos: ");
+            numeroSegundos = int.Parse(Console.ReadLine());
+
+            horas = numeroSegundos / 3600;
+            resto = numeroSegundos % 3600;
+            minutos = resto / 60;
+            segundos = resto % 60;
+
+            Console.WriteLine("Você trabalhou: " + horas + ":" + minutos + ":" + segundos + "h.");
+        }
+
+    }
+
+}
