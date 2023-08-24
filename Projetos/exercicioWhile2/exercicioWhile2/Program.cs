d2604ba2a8aa5daca06960b3c34c0485dfd19fca
WelleyIgor <122416707+WelleyIgor@users.noreply.github.com>
2023-08-23 20:10:50 -0400

    Add files via upload

---
+++ Projetos/exercicioWhile2/exercicioWhile2/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Globalization;
+
+namespace exercicioWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double somaIdades, idades, quantidadeIdades, mediaTurma;
+
+            Console.WriteLine("Digite as idades dos alunos da turma: ");
+            idades = double.Parse(Console.ReadLine());
+
+            somaIdades = 0;
+            quantidadeIdades = 0;
+            while (idades >= 0)
+            {
+                somaIdades += idades;
+                quantidadeIdades += 1;
+                idades = int.Parse(Console.ReadLine());
+            }
+            if (quantidadeIdades == 0)
+            {
+                Console.WriteLine("Impossivel Calcular...");
+            }
+            else
+            {
+                mediaTurma = somaIdades / quantidadeIdades;
+                Console.WriteLine("A média da turma é " + mediaTurma.ToString("F2", CultureInfo.InvariantCulture) + ".");
+            }
+        }
+    }
+}
