a5db097bc2698aedfbb9db1fb6025b6202eaaade
WelleyIgor <122416707+WelleyIgor@users.noreply.github.com>
2023-08-23 20:09:08 -0400

    Add files via upload

---
+++ Projetos/treinamentoEnquanto/treinamentoEnquanto/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace treinamentoEnquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero, somaTotal;
+
+            Console.WriteLine("Digite numeros a serem somados!!!");
+            numero = double.Parse(Console.ReadLine());
+
+            somaTotal = 0;
+
+            while (numero != 0)
+            {
+                somaTotal +=  numero;
+                numero = double.Parse(Console.ReadLine());  
+            }
+
+            Console.WriteLine("A soma de todos os numeros digitados acima é: " + somaTotal + ".");
+        }
+    }
+}
