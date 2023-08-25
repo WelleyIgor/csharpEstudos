004ebc2b9cc7c75a524a6f9358de0df5cb11baaa
WelleyIgor <122416707+WelleyIgor@users.noreply.github.com>
2023-08-24 20:38:07 -0400

    Add files via upload

---
+++ Projetos/testeFor/testeFor/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace testeFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numDefinicao, numSoma;
+
+            Console.WriteLine("Digite a quantidade de numeros que deseja inserir na soma: ");
+            numDefinicao = int.Parse(Console.ReadLine());
+            numSoma = 0;
+
+            for (int numCalc = 1; numCalc <= numDefinicao; numCalc ++)
+            {
+                Console.WriteLine("Certo, o numero de repetições será " + numDefinicao + ".");
+                Console.WriteLine("Digite o " + numCalc + "º numero");
+                numSoma += int.Parse(Console.ReadLine());
+                
+            }
+
+            Console.WriteLine("A Soma dos numeros digitados foi: " + numSoma);
+        }
+    }
+}
